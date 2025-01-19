28815d467d6118d8a0eb169b6c259331df58e384
Minhi98 <mbrolly98@gmail.com>
2025-01-19 20:25:20 +0000

    init

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const PROJECT_MOON = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+PROJECT_MOON.abilities = {
+  str: 'PROJECT_MOON.Ability.Str.long',
+  dex: 'PROJECT_MOON.Ability.Dex.long',
+  con: 'PROJECT_MOON.Ability.Con.long',
+  int: 'PROJECT_MOON.Ability.Int.long',
+  wis: 'PROJECT_MOON.Ability.Wis.long',
+  cha: 'PROJECT_MOON.Ability.Cha.long',
+};
+
+PROJECT_MOON.abilityAbbreviations = {
+  str: 'PROJECT_MOON.Ability.Str.abbr',
+  dex: 'PROJECT_MOON.Ability.Dex.abbr',
+  con: 'PROJECT_MOON.Ability.Con.abbr',
+  int: 'PROJECT_MOON.Ability.Int.abbr',
+  wis: 'PROJECT_MOON.Ability.Wis.abbr',
+  cha: 'PROJECT_MOON.Ability.Cha.abbr',
+};
